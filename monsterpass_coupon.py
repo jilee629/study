48184d35ac490d7008549a6a1e001567f13ad9b0
jilee629 <jilee629@gmail.com>
2022-11-10 19:12:49 +0900

    t

--- monsterpass_coupon.py
+++ monsterpass_coupon.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+from bs4 import BeautifulSoup
 import time
 
 options = Options()
@@ -12,22 +14,24 @@ options.add_experimental_option('detach', True)
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 options.add_argument("--start-maximized")
 
-service = Service(ChromeDriverManager().install())
-
-driver = webdriver.Chrome(service=service, options=options)
-
-# login
 id = input('Your ID: ')
 password = input('Password: ')
 
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+
+# login
 driver.get('https://partner.monpass.im/')
 driver.find_element(By.NAME, 'id').send_keys(id)
 driver.find_element(By.NAME, 'pw').send_keys(password)
 driver.find_element(By.CSS_SELECTOR, '.BU2.border.sc-bdVaJa.djlRTQ').click()
 time.sleep(1)
 
+# membership
 driver.get('https://partner.monpass.im/member')
-time.sleep(3)
+time.sleep(5)
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+print(soup.text)
 
-element = driver.find_element(By.CSS_SELECTOR, '.sc-jzJRlG.ipeLkB')
-element.send_keys(Keys.END)
+# driver.close()
