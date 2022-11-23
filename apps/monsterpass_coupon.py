af45fb83f063d60838ef56ca93f524f92f3c235e
jilee629 <jilee629@gmail.com>
2022-11-23 12:43:28 +0900

    2022-11-23

--- apps/monsterpass_coupon.py
+++ apps/monsterpass_coupon.py
@@ -1,11 +1,8 @@
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-
 from bs4 import BeautifulSoup
 import time
 
@@ -14,26 +11,68 @@ options.add_experimental_option('detach', True)
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 options.add_argument("--start-maximized")
 
-id = input('Your ID: ')
-password = input('Password: ')
-
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
+driver.implicitly_wait(5)
 
 # login
+
+# ID, Password 입력받기
+# id = input('Your ID: ')
+# password = input('Password: ')
+
+# main page
 driver.get('https://partner.monpass.im/')
-driver.find_element(By.NAME, 'id').send_keys(id)
-driver.find_element(By.NAME, 'pw').send_keys(password)
+# driver.find_element(By.NAME, 'id').send_keys(id)
+# driver.find_element(By.NAME, 'pw').send_keys(passwd)
+driver.find_element(By.CSS_SELECTOR, '.ui-input-text input[name="id"]').send_keys(id)
+driver.find_element(By.CSS_SELECTOR, '.ui-input-text input[name="pw"]').send_keys(passwd)
 driver.find_element(By.CSS_SELECTOR, '.BU2.border.sc-bdVaJa.djlRTQ').click()
 time.sleep(1)
 
-# membership
+# 회원관리 페이지
 driver.get('https://partner.monpass.im/member')
-time.sleep(5)
-html = driver.page_source
-soup = BeautifulSoup(html, 'html.parser')
-print(soup.text)
+time.sleep(1)
 
-element = driver.find_element(By.CSS_SELECTOR, '.sc-jzJRlG.ipeLkB')
-element.send_keys(Keys.END)
+# 더보기 클릭해주기
+while True:
+    try:
+        driver.find_element(By.CSS_SELECTOR, '.sc-cBdUnI.fBYDFC').click()
+        time.sleep(.1)
+    except:
+        break
+
+# member list 추출
+html = driver.page_source
+soup = BeautifulSoup(html, 'lxml')
+members = soup.select('.ui-repeat.sc-cSHVUG.keecsQ > a > p > strong')
+
+# 전화번호 추출
+phones = [member.text for member in members] 
+
+# 번호별 ticket 개수 추출
+def get_ticket(phone):
+    driver.find_element(By.NAME, 'search').send_keys(phone.replace('-',''))
+    # 검색 클릭
+    driver.find_element(By.CSS_SELECTOR, '.GY5.sc-kgoBCf.cxrvvx').click()
+    time.sleep(.5)
+    # 번호 클릭
+    driver.find_element(By.CSS_SELECTOR, '.ui-repeat.sc-cSHVUG.keecsQ').click()
+    time.sleep(.5)
+    # ticket = driver.find_element(By.XPATH, '//*[@id="root"]/main/div/article/aside/section/div[3]/button[2]/div/strong').text
+    ticket = driver.find_element(By.CSS_SELECTOR, '.sc-bMVAic.greGXT button:nth-of-type(2) div strong').text
+    driver.find_element(By.NAME, 'search').clear()
+    return ticket
+
+# 전화번호별 ticket 개수 검색
+tickets = [get_ticket(phone) for phone in phones]
+
+zip_list = zip(phones, tickets)
+# [print(z) for z in zip_list]
+print(len(list(zip_list)))
+
+driver.quit()
+
+
+
 
--- nadocoding/3_request.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-res = requests.get('http://naver.com')
-res.raise_for_status()
-
-print(res.text)
-
-with open("mynaver.html", "w", encoding="utf8") as f:
-    f.write(res.text)
