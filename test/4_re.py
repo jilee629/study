c42eb145aa8db3883ba171a61ad3f571017e4de2
jilee629 <jilee629@gmail.com>
2022-12-01 17:08:45 +0900

    a

--- test/4_re.py
+++
@@ -1,7 +0,0 @@
-import re
-
-p = re.compile("ca.e")
-
-m = p.match("case")
-
-print(m.group())
--- test/pandas_excel.py
+++ test/pandas_excel.py
@@ -1,21 +1,23 @@
 import pandas as pd
 from datetime import date
 
+# dict
 data = {
    'name' : ['A', 'B', 'C'],
    'id' : ['a', 'b', 'c'],
    'age' : ['11', '12', '13']
 }
 index = ['1', '2', '3']
-
 df = pd.DataFrame(data, index=index)
 df.to_excel('pandas1.xlsx', engine='openpyxl')
 
+# list나 tuple
 data2 = [
    ('A', 'a', '11'),
    ('B', 'b', '12'),
    ('C', 'c', '13')
 ]
 columns = ['name', 'id', 'age']
-df = pd.DataFrame(data2, columns=columns)
+index = ['1', '2', '3']
+df = pd.DataFrame(data2, columns=columns, index=index, )
 df.to_excel('pandas2.xlsx', engine='openpyxl')
