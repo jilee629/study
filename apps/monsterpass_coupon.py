cf93bd29a054f0858ed9945895888741992d2a00
jilee629 <jilee629@gmail.com>
2022-12-17 02:38:32 +0900

    add token fun

--- apps/monsterpass_coupon.py
+++ apps/monsterpass_coupon.py
@@ -11,7 +11,7 @@ import pandas as pd
 options = Options()
 options.add_experimental_option('detach', True)
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-options.add_argument("headless")
+# options.add_argument("headless")
 options.add_argument("--start-maximized")
 service = Service(ChromeDriverManager().install())
 
@@ -37,8 +37,16 @@ def more_click():
         try:
             driver.find_element(By.CSS_SELECTOR, '.sc-cBdUnI.fBYDFC').click()
             time.sleep(.1)
+            print(".", end="")
         except:
+            print("")
             break
+
+# token 가져오기
+def get_token():
+    token = driver.execute_script("return localStorage.getItem('token')")
+    print(f"token: {token}")
+    return token
 
 # member list 추출
 def get_members(soup):
@@ -57,11 +65,14 @@ def get_tickets(phones, token):
         'token': token,
     }
     tickets = []
-    for p in phones:
+    for i, p in enumerate(phones):
         url = "https://api.monpass.im/api/crm/users/phone/" + p.replace('-','') + "/"
         res = requests.get(url, headers=headers)
         res_data = res.json()
         tickets.append(res_data['data']['ticket'])
+        if i%10 == 0:
+            print(".", end="")
+    print("")
     return list(zip(phones, tickets))
 
 # 티켓이 있는 사용자만 추출하기
@@ -94,8 +105,8 @@ def save_to_excel(data, col, prefix):
     t = datetime.now()
     df = pd.DataFrame(data, columns=col)
     df.index += 1
-    fname = f"{t.year}{t.month}{t.day}_{t.hour}{t.minute}{t.second}"
-    df.to_excel(f'{prefix}_{fname}.xlsx', header=True, engine='openpyxl')
+    fdate = f"{t.year}{t.month}{t.day}_{t.hour}_{t.minute}"
+    df.to_excel(f"{fdate}_{prefix}.xlsx", header=True, engine='openpyxl')
 
 
 if __name__ == "__main__":
@@ -104,10 +115,8 @@ if __name__ == "__main__":
     id = input('Your ID: ')
     passwd = input('Password: ')
 
-    # token을 추출하는 방법이 필요함. 현재는 직접 입력
-    token = '9f8ef675397e6f48ebc07a9cd1936c1581f0786735ad450a4f8837f678f4aa70'
-
     page_login(url, id, passwd)
+    token = get_token()
     more_click()
 
     html = driver.page_source
@@ -115,37 +124,30 @@ if __name__ == "__main__":
 
     # 전체회원수 읽어오기
     t_member = soup.select_one('.sc-iFMziU.gNKmAv').text
-    print("Total members: ", t_member[0:-1])
+    print(f"Total members: {t_member[0:-1]}")
 
     # 명단에서 phone 번호만 추출하기
     phones = get_members(soup)
-    print("Total phones: ", len(phones))
+    print(f"Total phones: {len(phones)}")
 
     # phone에 할당된 ticket 전체 개수 가져오기
     tickets = get_tickets(phones, token)
-    print("Total tickets: ", len(tickets))
+    print(f"Verified users: {len(tickets)}")
 
-    # ticket 가진 사용자만 ticket 전체 개수 추출하기
+    # ticket 가진 사용자만 전체 개수 추출하기
     user_tickets = get_user_tickets(tickets)
-    print("User ticket: ", len(user_tickets))
-    
+    print(f"Ticket users: {len(user_tickets)}")
+
     # ticket 가진 사용자만 시간별 개수 추출하기
     user_detail_tickets = get_detail_tickets(user_tickets, token)
-    print("User detail tickets: ", len(user_detail_tickets))
+    print(f"Detail user tickets: {len(user_detail_tickets)}")
     
     # 브라우저 닫기
     driver.quit()
 
-
     #엑셀 저장
     col = ['Phone', 'Total ticket']
-
-    # Total tickets
-    # save_to_excel(tickets, col, f"total_{len(tickets)}")
-
-    # User tickets
-    # save_to_excel(user_tickets, col, f"user_{len(user_tickets)}") 
-
-    # # User detail tickets
+    save_to_excel(tickets, col, f"total_{len(tickets)}")
     col = None
     save_to_excel(user_detail_tickets, col, f"detail_{len(user_detail_tickets)}")
+    
