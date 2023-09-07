0ac8d5c8abd4b924ad2bd31abc046f7edc596cec
jilee <jilee629@gmail.com>
2023-09-07 19:02:43 +0900

    merge

--- test/test_python.py
+++ test/test_python.py
@@ -1,141 +1,120 @@
-def test_zip_1():
+def sep(num):
+    print(str(num), '-' * 80)
+
+def test_zip():
     numbers = [1, 2, 3]
     letters = ["A", "B", "C"]
     data = zip(numbers, letters)
+    
+    sep(1)
     print(type(data))
-    [print(d) for d in data]
-"""
-<class 'zip'>
-(1, 'A')
-(2, 'B')
-(3, 'C')
-"""
+    # <class 'zip'>
+    
+    sep(2)
+    print(data)
+    # <zip object at 0x00000234FFEA0480>
 
-def test_zip_2():
-    numbers = [1, 2, 3]
-    letters = ["A", "B", "C"]
+    sep(3)
+    [print(d) for d in data]
+    # (1, 'A')
+    # (2, 'B')
+    # (3, 'C')
+    
+    sep(4)
     data = list(zip(numbers, letters))
     print(data)
-"""
-[(1, 'A'), (2, 'B'), (3, 'C')]
-"""
+    # [(1, 'A'), (2, 'B'), (3, 'C')
 
-def ztest_zip_3():
-    numbers = [1, 2, 3]
-    letters = ["A", "B", "C"]
+    sep(5)
     data = dict(zip(numbers, letters))
     print(data)
-"""
-{1: 'A', 2: 'B', 3: 'C'}
-"""
+    # {1: 'A', 2: 'B', 3: 'C'}
 
-# unzip
-def test_zip_4():
-    numbers = [1, 2, 3]
-    letters = ["A", "B", "C"]
+    sep(6)
     data = list(zip(numbers, letters))
-    print(data)
-    numbers, letters = zip(*data)
-    print(numbers, letters)
-"""
-[(1, 'A'), (2, 'B'), (3, 'C')]
-(1, 2, 3) ('A', 'B', 'C')
-"""
+    num, let = zip(*data)
+    print(num, let)
+    # (1, 2, 3) ('A', 'B', 'C')
 
-# 병렬처리
-def test_zip_5():
+    sep(7)
     for number, upper, lower in zip("12345", "ABCDE", "abcde"):
         print(number, upper, lower)
-"""
-1 A a
-2 B b
-3 C c
-4 D d
-5 E e
-"""
+    # 1 A a
+    # 2 B b
+    # 3 C c
+    # 4 D d
+    # 5 E e
 
-def test_list_1():
+def test_list():
+    sep(1)
     list1 = ['a', 'b', 'c']
     list2 = ['e', 'f', 'g']
     list1.append(list2)
-    print(list1)    
-"""
-list1 = ['a', 'b', 'c']
-list2 = ['e', 'f', 'g']
-list1.append(list2)
-print('append:', list1)
-"""
+    print(list1)
+    # ['a', 'b', 'c', ['e', 'f', 'g']]    
 
-def test_list_2():
+    sep(2)
     list1 = ['a', 'b', 'c']
     list2 = ['e', 'f', 'g']
     list1.extend(list2)
     print(list1)
-"""
-list3 = ['a', 'b', 'c']
-list4 = ['e', 'f', 'g']
-list3.extend(list4)
-print('extend:', list3)
-"""
+    # ['a', 'b', 'c', ['e', 'f', 'g']]
 
-def test_request_1():
-    import requests
-    res = requests.get("https://jsonplaceholder.typicode.com/users/1")
-    print(res)
-    print(res.content)
-    print(res.headers)
-    print('\nContent-Type: ', res.headers['Content-Type'])
-
-def test_enum_1():
+def test_enum():
+    sep(1)
     for i, letter in enumerate(['A', 'B', 'C']):
         print(i, letter)   
-"""
-0 A
-1 B
-2 C
-"""
+    # 0 A
+    # 1 B
+    # 2 C
 
-def test_enum_2():
+    sep(2)
     for entry in enumerate(['A', 'B', 'C']):
         print(entry)
-"""
-(0, 'A')
-(1, 'B')
-(2, 'C')
-"""
+    # (0, 'A')
+    # (1, 'B')
+    # (2, 'C')
 
-def test_datetime_1():
+def test_datetime():
     from datetime import datetime
     import time
+
+    sep(1)
     print(datetime.now())
     # 2023-09-07 17:43:45.092355
     
+    sep(2)
     t = datetime.now()
     print(t.year, t.month, t.day, t.hour, t.minute, t.second)
     # 2023 9 7 17 43 45
 
-
-def test_datetime_2():
-    from datetime import datetime
-    import time
-
+    sep(3)
     print(time.time())
     # 1694076225.113485
     
+    sep(4)
     print(time.ctime())
     # Thu Sep  7 17:43:45 2023
 
+def test_request_1():
+    import requests
+    res = requests.get("https://jsonplaceholder.typicode.com/users/1")
+
+    sep(1)
+    print(res)
+
+    sep(2)
+    print(res.content)
+
+    sep(3)
+    print(res.headers)
+
+    sep(4)
+    print(res.headers['Content-Type'])
+
+
+
 if __name__ == "__main__":
-    # test_zip_1()
-    # test_zip_2()
-    # test_zip_3()
-    # test_zip_4()
-    # test_zip_5()
-    # test_list_1()
-    # test_list_2()
-    # test_enum_2()
-    # test_request_1()
-    # test_enum_1():
-    # test_enum_2():
-    # test_datetime_1()
-    test_datetime_2()
+    import sys
+    test = getattr(sys.modules[__name__], sys.argv[1])
+    test()
