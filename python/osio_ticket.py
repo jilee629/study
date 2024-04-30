6df50f212a7026fb808d387712fb28fe17da9594
jilee629 <jilee629@gmail.com>
2024-04-30 19:37:26 +0900

    none

--- python/osio_ticket.py
+++ python/osio_ticket.py
@@ -9,7 +9,8 @@ import requests
 from tqdm import tqdm
 import time
 from datetime import datetime, timedelta
-
+import asyncio
+import aiohttp
 
 def get_driver():
     service = Service(ChromeDriverManager().install())
@@ -48,44 +49,59 @@ def get_token():
 def send_query(url):
     headers = {
         'Authorization': 'Bearer ' + token,
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0',
     }
     res = requests.get(url, headers=headers)
     res.raise_for_status()
     return res.json()
 
-def get_shop_user_no(data):
-    url = "https://osio-api.peoplcat.com/shop/osio/user/search?type=phone&phone=" + str(data)
+def query_shop_user_no(phone):
+    url = "https://osio-api.peoplcat.com/shop/osio/user/search?type=phone&phone=" + phone
     res_data = send_query(url)
     shop_user_no = res_data['shop_users'][0]['shop_user_no']
-    return shop_user_no
+    return str(shop_user_no)
 
-def get_entry_time(data):
-    url = "https://osio-api.peoplcat.com/shop/v2/user/entry/log?shop_user_no=" + str(data)
+def query_user_no_shop_user_no(phone):
+    url = "https://osio-api.peoplcat.com/shop/osio/user/search?type=phone&phone=" + phone
     res_data = send_query(url)
-    try:
-        return res_data['log'][0]['entry_datetime']
-    except:
-        return None
+    shop_user_no = res_data['shop_users'][0]['shop_user_no']
+    user_no = res_data['shop_users'][0]['user_no']
+    return str(user_no), str(shop_user_no)
 
-def get_last_visit(data):
+def query_visit_count(user_no, shop_user_no):
+    url = "https://osio-api.peoplcat.com/shop/user/summary/data?user_no=" + user_no + "&shop_user_no=" + shop_user_no
+    res_data = send_query(url)
+    return res_data['visit_count']
+
+def query_entry_datetime(shop_user_no):
+    url = "https://osio-api.peoplcat.com/shop/v2/user/entry/log?shop_user_no=" + shop_user_no
+    res_data = send_query(url)
+    return res_data['log'][0]['entry_datetime']
+
+def get_visit_count(phone):
+    visit_count = list()
+    for p in tqdm(phone, desc='visit_count'):
+        user_no , shop_user_no = query_user_no_shop_user_no(p)
+        user_visit_count = query_visit_count(user_no, shop_user_no)
+        visit_count.append(user_visit_count)
+    return visit_count
+
+def get_entry_datetime(visit_count, phone):
     last_visit = list()
-    for phone in tqdm(data):
-        shop_user_no = get_shop_user_no(phone)
-        entry_time = get_entry_time(shop_user_no)
-        last_visit.append(entry_time)
+    for v, p in zip(tqdm(visit_count, desc='entry_datetime'), phone):
+        if v == 0:
+            last_visit.append(None)
+        else:
+            shop_user_no= query_shop_user_no(p)
+            entry_time = query_entry_datetime(shop_user_no)
+            last_visit.append(entry_time)
     return last_visit
-
-def save_to_excel(data):
-    df = pd.DataFrame(data)
-    df.index += 1
-    fdate = datetime.now().strftime("%Y%m%d%H%M")
-    df.to_excel(f"{fdate}.xlsx", engine='openpyxl')
 
 
 if __name__ == "__main__":
 
     start = time.time()
-    
+
     driver = get_driver()
     credit = get_credit()
     page_login(credit[0], credit[1])
@@ -98,26 +114,23 @@ if __name__ == "__main__":
     cs_ticket_name = df['오시오명'].values.tolist()
     cs_ticket_count = df['오시오 잔여값'].values.tolist()
     cs_ticket_expired = df['오시오 만료일'].values.tolist()
-    cs_last_visit = get_last_visit(cs_phone)
-
+    cs_visit_count = get_visit_count(cs_phone)
+    cs_entry_datetime = get_entry_datetime(cs_visit_count, cs_phone)
     cs_data = {
                 'phone' : cs_phone,
                 'ticket_name' : cs_ticket_name,
                 'ticket_count' : cs_ticket_count,
                 'ticket_expired' : cs_ticket_expired,
-                'last_visited' : cs_last_visit,
+                'visit_count' : cs_visit_count,
+                'entry_datetime' : cs_entry_datetime,
             }
-
-    print("lenth : ", 
-          len(cs_data['phone']),
-          len(cs_data['ticket_name']),
-          len(cs_data['ticket_count']),
-          len(cs_data['ticket_expired']),
-          len(cs_data['last_visited']),
-          )
-
-    save_to_excel(cs_data)
+    [print(len(val)) for val in cs_data.values()]
     driver.quit()
 
+    df = pd.DataFrame(cs_data)
+    fdate = datetime.now().strftime("%Y%m%d%H%M")
+    df.to_excel(f"{fdate}.xlsx", engine='openpyxl')
+    
     delta = time.time() - start
     print(f"-> Elapsed time : {timedelta(seconds=delta)}")
+
--- test/test_pandas_df.py
+++ test/test_pandas_df.py
@@ -3,17 +3,19 @@ import pandas as pd
 # dict data 일경우, key는 columns에 value는 index에 들어감
 data = {'name' : ['A', 'B', 'C'], 'id' : ['a', 'b', 'c'], 'age' : ['11', '12', '13']}
 df = pd.DataFrame(data)
-print(df)
+# print(df)
 # name id age
 # 0    A  a  11
 # 1    B  b  12
 # 2    C  c  13
+print(df["age"][1])
+print(df.loc[1]["age"])
 
 data = [['A', 'a', '11'], ['B', 'b', '12'], ['C', 'c', '13']]
 columns = ['name', 'id', 'age']
 index = ['1', '2', '3']
 df = pd.DataFrame(data, columns=columns, index=index)
-print(df)
+# print(df)
 '''
 name id age
 1    A  a  11
@@ -21,12 +23,11 @@ name id age
 3    C  c  13
 '''
 
-
 data2 = [('A', 'a', '11'), ('B', 'b', '12'), ('C', 'c', '13')]
 columns = ['name', 'id', 'age']
 index = ['1', '2', '3']
 df = pd.DataFrame(data, columns=columns, index=index)
-print(df)
+# print(df)
 '''
 name id age
 1    A  a  11
