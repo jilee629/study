f69689d77a89fce139e60bb5e39e3339910eaffa
jilee629 <jilee629@gmail.com>
2024-05-04 18:47:13 +0900

    none

--- test/crawling_speed.py
+++ test/crawling_speed.py
@@ -8,44 +8,48 @@ import aiohttp
 
 def t_urllib(curl, n):
     start = time.time()
+    result =  list()
     for i in range(1,n):
         response = urllib.request.urlopen(url + str(i)).read()
-        result = eval(response.decode('utf-8'))['title']
-        # print(result)
-    print(f"-> urllib : {time.time()-start}")
+        result.append(eval(response.decode('utf-8'))['title'])
+    print(f"\n-> urllib : {time.time()-start}")
+    print(result)
 
 def t_http(n):
     start = time.time()
+    result =  list()
     for i in range(1,n):
         conn = http.client.HTTPSConnection("jsonplaceholder.typicode.com")
         conn.request("GET", "/posts/" + str(i))
         response = conn.getresponse().read()
-        result = eval(response.decode('utf-8'))['title']
-        # print(result)
-    print(f"-> http.client : {time.time()-start}")
+        result.append(eval(response.decode('utf-8'))['title'])
+    print(f"\n-> http.client : {time.time()-start}")
+    print(result)
 
 def t_requests(curl, n):
     start = time.time()
+    result =  list()
     for i in range(1, n):
         response = requests.get(url + str(i))
-        result = response.json()['title']
-        # print(result)
-    print(f"-> request : {time.time()-start}")
+        result.append(response.json()['title'])
+    print(f"\n-> request : {time.time()-start}")
+    print(result)
 
 def t_urllib3(curl, n):
     start = time.time()
+    result =  list()
     http = urllib3.PoolManager()
     for i in range(1, n):
         response = http.request('GET', url + str(i))
-        result = response.json()['title']
-        # print(result)
-    print(f"-> urllib3 : {time.time()-start}")
+        result.append(response.json()['title'])
+    print(f"\n-> urllib3 : {time.time()-start}")
+    print(result)
 
 async def fetch(session, url):
     async with session.get(url) as response:
         response = await response.json()
         result = response['title']
-        # print(result)
+        return result
 
 async def main(url, n):
     start = time.time()
@@ -53,19 +57,21 @@ async def main(url, n):
         tasks = list()
         for i in range(1, n):
             tasks.append(fetch(session, url + str(i)))
-        await asyncio.gather(*tasks)
-    print(f"-> urllib3 : {time.time()-start}")
+        result = await asyncio.gather(*tasks)
+        print(f"\n-> aiphttp : {time.time()-start}")
+        print(result)
 
 if __name__ == "__main__":
 
     url = "https://jsonplaceholder.typicode.com/posts/"
-    n = 101
+    n = 11
 
+    asyncio.run(main(url, n))
+    t_urllib3(url, n)
     t_requests(url, n)
     t_urllib(url, n)
-    t_urllib3(url, n)
     t_http(n)
-    asyncio.run(main(url, n))
+
 
         
 
--- test/test_pandas_df.py
+++ test/test_pandas_df.py
@@ -1,36 +1,43 @@
 import pandas as pd
 
-# dict data 일경우, key는 columns에 value는 index에 들어감
-data = {'name' : ['A', 'B', 'C'], 'id' : ['a', 'b', 'c'], 'age' : ['11', '12', '13']}
-df = pd.DataFrame(data)
-# print(df)
-# name id age
-# 0    A  a  11
-# 1    B  b  12
-# 2    C  c  13
-print(df["age"][1])
-print(df.loc[1]["age"])
+def test(i):
+    if i == 1:
+        data = {'name' : ['A', 'B', 'C'], 'id' : ['a', 'b', 'c'], 'age' : ['11', '12', '13']}
+        df = pd.DataFrame(data)
+        print(df)
+        # name id age
+        # 0    A  a  11
+        # 1    B  b  12
+        # 2    C  c  13
+        print(df["age"][1])
+        # 12
+        print(df.loc[1]["age"])
+        # 12
+    
+    if i == 2:
+        data = [['A', 'a', '11'], ['B', 'b', '12'], ['C', 'c', '13']]
+        columns = ['name', 'id', 'age']
+        index = ['1', '2', '3']
+        df = pd.DataFrame(data, columns=columns, index=index)
+        print(df)
+        #   name id age
+        # 1    A  a  11
+        # 2    B  b  12
+        # 3    C  c  13
 
-data = [['A', 'a', '11'], ['B', 'b', '12'], ['C', 'c', '13']]
-columns = ['name', 'id', 'age']
-index = ['1', '2', '3']
-df = pd.DataFrame(data, columns=columns, index=index)
-# print(df)
-'''
-name id age
-1    A  a  11
-2    B  b  12
-3    C  c  13
-'''
+    if  i == 3:
+        data = [('A', 'a', '11'), ('B', 'b', '12'), ('C', 'c', '13')]
+        columns = ['name', 'id', 'age']
+        index = ['1', '2', '3']
+        df = pd.DataFrame(data, columns=columns, index=index)
+        print(df)
+        #   name id age
+        # 1    A  a  11
+        # 2    B  b  12
+        # 3    C  c  13
+        
+        
+if __name__ == "__main__":
+    
+    test(3)
 
-data2 = [('A', 'a', '11'), ('B', 'b', '12'), ('C', 'c', '13')]
-columns = ['name', 'id', 'age']
-index = ['1', '2', '3']
-df = pd.DataFrame(data, columns=columns, index=index)
-# print(df)
-'''
-name id age
-1    A  a  11
-2    B  b  12
-3    C  c  13
-'''
