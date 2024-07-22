d1af2246c724c40fca820e7b174a2cd35e5ba591
jilee629 <jilee629@gmail.com>
2024-07-22 13:44:27 +0000

    test

--- test/test_webdrive.py
+++ test/test_webdrive.py
@@ -2,14 +2,9 @@ from selenium import webdriver
 from pyvirtualdisplay import Display
 
 
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
-def firefox():
-    driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
-
-
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+
 def chrome():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
@@ -17,14 +12,23 @@ def chrome():
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
+
 def chromium():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
 
+
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+
+def firefox():
+    driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
 if __name__ == "__main__":
     
     display = Display(visible=0, size=(1024, 768))
     display.start()
     
+    # chrome()
+    # chromium()
     firefox()
 
