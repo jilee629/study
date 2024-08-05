770c94eae7b0ef01c00ffa97be5b4b09d9da7aee
jilee629 <jilee629@gmail.com>
2024-08-05 16:14:51 +0900

    a

--- python/osio_info.py
+++ python/osio_info.py
@@ -14,6 +14,76 @@ from datetime import datetime, timedelta
 import urllib3
 import os
 
+def get_driver():
+    service = Service(ChromeDriverManager().install())
+    options = Options()
+    options.add_argument("--start-maximized")
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_experimental_option("prefs", {"download.default_directory": "/home/ubuntu/log"})
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.implicitly_wait(10)
+    return driver
+
+def get_credit():
+    credit = os.path.dirname(__file__) + '/credit.toml'
+    with open(credit, 'rb') as f:
+        data = tomllib.load(f)
+        username = data['osio']['username']
+        password = data['osio']['password']
+    return username, password
+
+def enter_login(username, password):
+    # login page
+    url = "https://osio-shop.peoplcat.com/login"
+    driver.get(url)
+    driver.find_element(By.XPATH, '//*[@id="root"]/form/div/div[1]/input').send_keys(username)
+    driver.find_element(By.XPATH, '//*[@id="root"]/form/div/div[2]/input').send_keys(password)
+    driver.find_element(By.XPATH, '//*[@id="root"]/form/div/button').click()
+    # manager mode
+    driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div/div[2]/button[2]').click()
+    return
+
+def download_csinfo():
+    driver.get("https://osio-shop.peoplcat.com/admin/users")
+    driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div[1]/div[1]/button').click()
+    driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div[2]/div/div/div[2]/div[2]/button').click()
+    driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/button[2]').click()
+    time.sleep(5)
+    return
+
+def get_token():
+    token = driver.execute_script("return localStorage.getItem('access_token')")
+    print(f"-> Token: {token}")
+    return token
+
+def get_cs_user_shop_no(cs_phone):
+    cs_shop_user_no = list()
+    cs_user_no = list()   
+    for phone in tqdm(cs_phone, desc='user_no'):
+        result = get_shop_user_no(phone)
+        cs_shop_user_no.append(result[0])
+        cs_user_no.append(result[1])
+    return cs_shop_user_no, cs_user_no
+
+def get_cs_entry_datetime(cs_shop_user_no):
+    cs_entry_datatime = list()
+    for shop_user_no in tqdm(cs_shop_user_no, desc='entry_datatime'):
+        cs_entry_datatime.append(get_entry_datetime(shop_user_no))
+    return cs_entry_datatime
+
+def get_cs_visit_count(cs_user_no, cs_shop_user_no):
+    cs_visit_count = list()
+    for user_no, shop_user_no in tqdm(zip(cs_user_no, cs_shop_user_no), total = len(cs_user_no), desc='visit_count'):
+        cs_visit_count.append(get_visit_count(user_no, shop_user_no))
+    return cs_visit_count
+
+def save_data(cs_data):
+    df = pd.DataFrame(cs_data)
+    fname = '/home/ubuntu/log/' + now.strftime('%Y%m%d_%H%M') + '_점핑몬스터 미사점_고객정보.xlsx'
+    df.to_excel(fname, engine='openpyxl')
+    return
 
 def fetch(url):
     time.sleep(0.5)
@@ -49,103 +119,48 @@ def get_visit_count(user_no, shop_usre_no):
 	visit_count = response.json()['visit_count']
 	return str(visit_count)
 
-now = datetime.now()
-print(now.strftime("%Y-%m-%d %H:%M:%S %A"))
 
-start = datetime.now().timestamp()
+if __name__ == "__main__":
 
-display = Display(visible=0, size=(1024, 768))
-display.start()
+    display = Display(visible=0, size=(1024, 768))
+    display.start()
 
-service = Service(ChromeDriverManager().install())
-options = Options()
-options.add_argument("--start-maximized")
-options.add_argument("--headless=new")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-prefs = {"download.default_directory": "/root/study/tmp"}
-options.add_experimental_option("prefs", prefs)
-driver = webdriver.Chrome(service=service, options=options)
-driver.implicitly_wait(10)
+    now = datetime.now()
+    print(now.strftime("%Y-%m-%d %H:%M:%S %A"))
+    start = datetime.now().timestamp()
 
-credit = os.path.dirname(__file__) + '/credit.toml'
-with open(credit, 'rb') as f:
-    data = tomllib.load(f)
-    username = data['osio']['username']
-    password = data['osio']['password']
+    driver = get_driver()
+    username, password = get_credit()
+    enter_login(username, password) 
+    download_csinfo()
+    token = get_token()
 
-# login page
-driver.get("https://osio-shop.peoplcat.com/login")
-print(f'-> {driver.current_url}')
-driver.find_element(By.XPATH, '//*[@id="root"]/form/div/div[1]/input').send_keys(username)
-driver.find_element(By.XPATH, '//*[@id="root"]/form/div/div[2]/input').send_keys(password)
-driver.find_element(By.XPATH, '//*[@id="root"]/form/div/button').click()
+    csfile = '/home/ubuntu/log/' + now.strftime('%Y%m%d') + '_점핑몬스터 미사점_고객정보.xlsx'
+    # df = pd.read_excel(csfile, dtype = 'str')
+    df = pd.read_excel(csfile, dtype = 'str', nrows = 10)
 
-# manager mode
-driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div/div[2]/button[2]').click()
-print(f'-> {driver.current_url}')
+    cs_phone = df['전화번호'].values.tolist()
+    cs_ticket_name = df['오시오명'].values.tolist()
+    cs_ticket_count = df['오시오 잔여값'].values.tolist()
+    cs_ticket_expired = df['오시오 만료일'].values.tolist()
+    cs_shop_user_no, cs_user_no = get_cs_user_shop_no(cs_phone)
+    cs_entry_datatime = get_cs_entry_datetime(cs_shop_user_no)
+    cs_visit_count = get_cs_visit_count(cs_user_no, cs_shop_user_no)
+    cs_data = {
+                'phone' : cs_phone,
+                'osio' : cs_ticket_name,
+                'count' : cs_ticket_count,
+                'expired' : cs_ticket_expired,
+                'shop_user_no' : cs_shop_user_no,
+                'user_no' : cs_user_no,
+                'entry' : cs_entry_datatime,
+                'visit' : cs_visit_count,
+            } 
 
-# user infomation excel download 
-driver.get("https://osio-shop.peoplcat.com/admin/users")
-print(f'-> {driver.current_url}')
-driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div[1]/div/div[1]/div[2]/button').click()
-driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div[1]/div[2]/div/div/div[2]/div[2]/button').click()
-driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div[1]/div[2]/div/div/div[2]/div[3]/div/button[2]').click()
-time.sleep(5)
+    [print(f"{key} : {len(value)}") for key, value in cs_data.items()]
+    save_data(cs_data)
+    print(f"-> Elapsed time : {timedelta(seconds=datetime.now().timestamp() - start)}")
 
-
-# get token
-token = driver.execute_script("return localStorage.getItem('access_token')")
-print(f"-> Token: {token}")
-
-# download cs information
-today = now.strftime('%Y%m%d', time.localtime())
-# df = pd.read_excel('../tmp/' + today + '_점핑몬스터 미사점_고객정보.xlsx', dtype = 'str')
-df = pd.read_excel('../tmp/' + today + '_점핑몬스터 미사점_고객정보.xlsx', dtype = 'str', nrows = 5)
-
-cs_phone = df['전화번호'].values.tolist()
-cs_ticket_name = df['오시오명'].values.tolist()
-cs_ticket_count = df['오시오 잔여값'].values.tolist()
-cs_ticket_expired = df['오시오 만료일'].values.tolist()
-
-cs_shop_user_no = list()
-cs_user_no = list()
-for phone in tqdm(cs_phone, desc='user_no'):
-    result = get_shop_user_no(phone)
-    cs_shop_user_no.append(result[0])
-    cs_user_no.append(result[1])
-
-cs_entry_datatime = list()
-for shop_user_no in tqdm(cs_shop_user_no, desc='entry_datatime'):
-    cs_entry_datatime.append(get_entry_datetime(shop_user_no))
-
-cs_visit_count = list()
-for user_no, shop_user_no in tqdm(zip(cs_user_no, cs_shop_user_no), total = len(cs_user_no), desc='visit_count'):
-    cs_visit_count.append(get_visit_count(user_no, shop_user_no))
-
-cs_data = {
-            'phone' : cs_phone,
-            'osio_name' : cs_ticket_name,
-            'osio_count' : cs_ticket_count,
-            'expired' : cs_ticket_expired,
-            'shop_user_no' : cs_shop_user_no,
-            'user_no' : cs_user_no,
-            'entry_datetime' : cs_entry_datatime,
-            'visit_count' : cs_visit_count,
-        }
-
-[print(f"{key} : {len(value)}") for key, value in cs_data.items()]
-
-df = pd.DataFrame(cs_data)
-fdate = now.strftime("%Y%m%d_%H%M")
-fname = os.path.dirname(__file__) + '/../tmp/' + fdate + '.xlsx'
-df.to_excel(fname, engine='openpyxl')
-
-print(f"-> Elapsed time : {timedelta(seconds=datetime.now().timestamp() - start)}")
-
-driver.quit()
-display.stop()
-
-
-
+    driver.quit()
+    display.stop()
 
