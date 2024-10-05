fa81a887def96f7188c180cc0bce383d452a8824
jilee629 <jilee629@gmail.com>
2024-10-05 23:39:28 +0900

    고객정보에 내용을 추가히기

--- test/test_pandas_df.py
+++ test/test_pandas_df.py
@@ -1,43 +1,22 @@
 import pandas as pd
 
-def test(i):
-    if i == 1:
-        data = {'name' : ['A', 'B', 'C'], 'id' : ['a', 'b', 'c'], 'age' : ['11', '12', '13']}
-        df = pd.DataFrame(data)
-        print(df)
-        # name id age
-        # 0    A  a  11
-        # 1    B  b  12
-        # 2    C  c  13
-        print(df["age"][1])
-        # 12
-        print(df.loc[1]["age"])
-        # 12
-    
-    if i == 2:
-        data = [['A', 'a', '11'], ['B', 'b', '12'], ['C', 'c', '13']]
-        columns = ['name', 'id', 'age']
-        index = ['1', '2', '3']
-        df = pd.DataFrame(data, columns=columns, index=index)
-        print(df)
-        #   name id age
-        # 1    A  a  11
-        # 2    B  b  12
-        # 3    C  c  13
+data = {'name' : ['A', 'B', 'C'], 'id' : ['a', 'b', 'c'], 'age' : ['11', '12', '13']}
+df = pd.DataFrame(data)
+print(df)
 
-    if  i == 3:
-        data = [('A', 'a', '11'), ('B', 'b', '12'), ('C', 'c', '13')]
-        columns = ['name', 'id', 'age']
-        index = ['1', '2', '3']
-        df = pd.DataFrame(data, columns=columns, index=index)
-        print(df)
-        #   name id age
-        # 1    A  a  11
-        # 2    B  b  12
-        # 3    C  c  13
-        
-        
-if __name__ == "__main__":
-    
-    test(3)
+# data = [['A', 'a', '11'], ['B', 'b', '12'], ['C', 'c', '13']]
+# columns = ['name', 'id', 'age']
+# index = ['1', '2', '3']
+# df = pd.DataFrame(data, columns=columns, index=index)
+# print(df)
+
+
+print(f"\n{df.values.tolist()}")
+
+print(f"\n{df['age']}")
+
+print(f"\n{df['age'][1]}")
+
+
+
 
