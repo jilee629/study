c08404028dfcca565381941076d7add9298092d5
jilee629 <jilee629@gmail.com>
2025-05-23 15:20:37 +0900

    .

--- python/osio.py
+++ python/osio.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from pyvirtualdisplay import Display
 import tomllib
 import os
 import time
@@ -32,30 +31,24 @@ def enter_login(driver, username, password):
     driver.get("https://osio-shop.peoplcat.com/login")
 
     # notice window
-    notice = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div/div/div/div[3]/button')
-    if notice is True:
-        notice.click()
+    popup = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div')
+    if popup is not None:
+        ever_chk = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div/div/div/div[3]/div').click()
+        notice_btn = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div/div/div/div[3]/button').click()
 
     # login
-    uname = driver.find_element(By.XPATH, '//*[@id="root"]/form/div[2]/div[1]/input')
-    uname.send_keys(username)
-    passwd = driver.find_element(By.XPATH, '//*[@id="root"]/form/div[2]/div[2]/input')
-    passwd.send_keys(password)
-    submit_button = driver.find_element(By.XPATH, '//*[@id="root"]/form/div[2]/button')
-    submit_button.click()
+    uname_box = driver.find_element(By.XPATH, '//*[@id="root"]/form/div[2]/div[1]/input').send_keys(username)
+    passwd_box = driver.find_element(By.XPATH, '//*[@id="root"]/form/div[2]/div[2]/input').send_keys(password)
+    submit_btn = driver.find_element(By.XPATH, '//*[@id="root"]/form/div[2]/button').click()
     print("Login success" )
     # select admin or user
-    admin_button = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div/div[2]/button[2]')
-    admin_button.click()
+    admin_btn = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div/div[2]/button[2]').click()
     return
 
 def quit_user(driver):
     driver.get("https://osio-shop.peoplcat.com/admin/settings")
-    quit_button = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[1]/div[1]/div[1]/div/div[1]/button')
-    print(quit_button.text)
-    quit_button.click()
-    confirm_button = driver.find_element(By.XPATH, '//*[@id="root"]/div[2]/div/div/footer/button[2]')
-    confirm_button.click()
+    quit_btn = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[1]/div[1]/div[1]/div/div[1]/button').click()
+    confirm_btn = driver.find_element(By.XPATH, '//*[@id="root"]/div[2]/div/div/footer/button[2]').click()
     time.sleep(3)
     return
 
@@ -68,12 +61,9 @@ def get_count(driver):
 
 def download_csinfo(driver):
     driver.get("https://osio-shop.peoplcat.com/admin/users")
-    csinfo_button = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div[1]/div[1]/button')
-    csinfo_button.click()
-    agree_button = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div[2]/div/div/div/div/div/button')
-    agree_button.click()
-    download_button = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div[2]/div/div/footer/button[2]')
-    download_button.click()
+    csinfo_btn = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div[1]/div[1]/button').click()
+    agree_chk = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div[2]/div/div/div/div/div/button').click()
+    download_btn = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div[2]/div/div/footer/button[2]').click()
     print("Starting download")
     time.sleep(10)
     return
