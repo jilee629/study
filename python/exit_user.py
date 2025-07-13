9e135cd0cc637b4f56ec1a5f6c78396d3d668d30
jilee629 <jilee629@gmail.com>
2025-07-14 01:26:58 +0900

    rename

---
+++ python/exit_user.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from pyvirtualdisplay import Display
+from datetime import datetime
+import osio
+import os
+
+
+if __name__ == "__main__":
+
+    if os.name != 'nt':
+        display = Display(visible=0, size=(1280, 1024))
+        display.start()
+
+    now = datetime.now()
+    print("\n", now.strftime("%Y-%m-%d %H:%M:%S %A"))
+
+    driver = osio.get_driver()
+
+    username, password = osio.get_credential()
+    osio.enter_login(driver, username, password)
+    
+    osio.get_count(driver)
+    osio.exit_user(driver)
+    osio.get_count(driver)
+
+    driver.quit()
+    if os.name != 'nt':
+        display.stop()
