f11a48d7299f599107e3530da357c70c853d9be0
jilee629 <jilee629@gmail.com>
2025-08-05 19:11:05 +0900

    none

--- python/upload_drive.py
+++ python/upload_drive.py
@@ -13,11 +13,12 @@ if __name__ == "__main__":
 
     folder_id = osio.create_drive_folder(fdate)
 
-    file_name = "len_" + fdate + "_점핑몬스터 미사점_고객정보.xlsx"
-    osio.upload_file(folder_id, file_name, 'xlsx')
-
     file_name = "exit_user.log"
     osio.upload_file(folder_id, file_name)
 
     file_name = "download_csinfo.log"
     osio.upload_file(folder_id, file_name)
+
+    file_name = "len_" + fdate + "_점핑몬스터 미사점_고객정보.xlsx"
+    osio.upload_file(folder_id, file_name, 'xlsx')
+
