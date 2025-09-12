d8af7c2fd32aced85a5f619c2061129c12934898
jilee629 <jilee629@gmail.com>
2025-09-12 16:21:02 +0900

    a

--- python/sample_user.py
+++ python/sample_user.py
@@ -6,10 +6,12 @@ import pandas as pd
 
 if __name__ == "__main__":
 
-    file_path = os.path.dirname(__file__) + '/../../log/20250910_점핑몬스터 미사점_고객정보.xlsx'
+    log_dir = os.path.dirname(__file__) + '/../../log/'
+    yesterday = datetime.now() - timedelta(days=1)
+    file_path = log_dir +  yesterday.strftime('%Y%m%d') + '_점핑몬스터 미사점_고객정보.xlsx'
     df = pd.read_excel(file_path, dtype = 'str')
     df_noticket = df.loc[df['오시오 잔여값'].isna()]
-    phone_list = random.sample(df_noticket["전화번호"].values.tolist(), 20)
+    phone_list = random.sample(df_noticket["전화번호"].values.tolist(), 100)
 
     if os.name != 'nt':
         display = Display(visible=0, size=(1920,1080))
@@ -27,9 +29,10 @@ if __name__ == "__main__":
         visit_count, oticket = osio.get_user_summary(user_no, shop_user_no, token)
         last_entry = osio.get_user_log(shop_user_no, token)
         data.append([phone, visit_count, oticket, last_entry])
-        time.sleep(5)
+        time.sleep(10)
     columns = ['phone', 'visit', 'oticket', 'entry']
     df = pd.DataFrame(data, columns=columns)
+    pd.set_option('display.max_rows', None)
     print(df)
 
     driver.quit()
--- test/python/test_datetime.py
+++ test/python/test_datetime.py
@@ -4,17 +4,22 @@ import time
 now = datetime.now()
 
 print(f"now : {now}")
+# now : 2025-09-12 15:53:22.269899
 
 print(f"date : {now.date()}")
+# date : 2025-09-12
 
 print(f"time : {now.time()}")
+# time : 15:53:22.269899
 
 print(f"strftime : {now.strftime('%Y-%m-%d %H:%M:%S %A')}")
+# strftime : 2025-09-12 15:53:22 Friday
 
 timestamp = now.timestamp()
 print(f"timestamp: {timestamp}")
+# timestamp: 1757660002.269899
 
-yesterday = now - timedelta(days=-1)
+yesterday = now - timedelta(days=1)
 print(f"timedelta: {yesterday}")
-
+# timedelta: 2025-09-11 15:53:22.269899
 
